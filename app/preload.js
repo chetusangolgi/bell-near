bcc50cd39e47fd8777b1d8e1400ab22de66e265e
cft-yamuna <yamuna@craftech360.com>
2025-10-29 18:42:26 +0530

    jfgvjh

--- app/preload.js
+++ app/preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   getVideoPath: (args) => ipcRenderer.invoke('get-video-path', args),
-  getDisplayName: () => ipcRenderer.invoke('get-display-name')
+  getDisplayName: () => ipcRenderer.invoke('get-display-name'),
+  getAudioDevice: () => ipcRenderer.invoke('get-audio-device')
 });
